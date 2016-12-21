fc09873e393618890a329cf6e32a56c4c24d4cdc
Johan Eriksson <joheri71@hotmail.com>
2016-12-21 14:50:45 +0100

    Added Comic Book Detail View

---
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        //Returnerar en view som har en url innehållande ComicBooks och Detail. 
+        //Den letar i Viewmappen och sökvägen blir ComicBooks/Detail
+        public ActionResult Detail()
+        {
+            return View();   
+        }
+    }
+}
