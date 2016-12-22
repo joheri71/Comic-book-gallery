6d2016f1a0557d91c4945b13f1d9b80fdf6750d9
Johan Eriksson <joheri71@hotmail.com>
2016-12-22 12:23:52 +0100

    Uppdated Comic Book Detail view to be strongly typed

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicBookGallery.Models;
 
 namespace ComicBookGallery.Controllers
 {
@@ -12,20 +13,39 @@ namespace ComicBookGallery.Controllers
         //Den letar i Viewmappen och sökvägen blir ComicBooks/Detail
         public ActionResult Detail()
         {
-            ViewBag.SeriesTitle = "The amzing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "Final issue! Witness the final hours of Doctor Octopus life and his one, last, great act of revenge!" +
-                              "Even if Spider-Man survives <strong>will Peter Parker?</strong>";
-            ViewBag.Artists = new string[]
+            //Instansierar ett objekt av klassen ComicBook och lägger till ett antal Artists i propertyn Artists
+            //som är en array av Artists
+            var comicBook = new ComicBook()
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+                SeriesTitle = "The amzing Spider-Man",
+                IssueNumber = 7,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus life and his one, last, great act of revenge!" +
+                                    "Even if Spider-Man survives <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Dan Slott", Role = "Script"},
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils"},
+                    new Artist() { Name = "Victor Olazaba", Role = "Inks"},
+                    new Artist() { Name = "Edgar Delgado", Role = "Colors"},
+                    new Artist() {Name = "Chris Eliopoulos", Role = "Letters" }
+                }
             };
+
+            //ViewBag.SeriesTitle = "The amzing Spider-Man";
+            //ViewBag.IssueNumber = 700;
+            //ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus life and his one, last, great act of revenge!" +
+            //                  "Even if Spider-Man survives <strong>will Peter Parker?</strong></p>";
+            //ViewBag.Artists = new string[]
+            //{
+            //    "Script: Dan Slott",
+            //    "Pencils: Humberto Ramos",
+            //    "Inks: Victor Olazaba",
+            //    "Colors: Edgar Delgado",
+            //    "Letters: Chris Eliopoulos"
+            //};
         
-            return View();   
+            
+            return View(comicBook);   
         }
     }
 }
